23fc10ad064679d670a72e3999213a11941769c9
Rafi Bagaskara <rafibauu@gagasimaji.com>
2022-09-03 21:01:25 +0700

    Episdeo 14

--- hooks/useQuery.js
+++ hooks/useQuery.js
@@ -1,18 +1,26 @@
 import { useEffect, useRef, useState, useCallback } from 'react'
-import { ref, get, orderByKey, orderByValue, query } from 'firebase/database'
+import { ref, get, orderByKey, orderByValue, query, orderByChild } from 'firebase/database'
 import { database } from '../services/firebase-sdk'
 
-const getOrder = (type) => {
+const getOrder = (type, child) => {
   switch (type) {
     case 'value':
       return orderByValue()
+    case 'child':
+      return orderByChild(child)
     case 'key':
     default:
       return orderByKey()
   }
 }
 
-const useQuery = ({ path, initialLoad = true, type = 'key', queries = [] }) => {
+const useQuery = ({
+  path,
+  initialLoad = true,
+  type = 'key',
+  child = '',
+  queries = []
+}) => {
   const [loading, setLoading] = useState(initialLoad)
   const snapshot = useRef(null)
   const error = useRef(null)
@@ -21,7 +29,7 @@ const useQuery = ({ path, initialLoad = true, type = 'key', queries = [] }) => {
   const queryValue = useCallback(async () => {
     try {
       const dbRef = ref(database, path)
-      const dbQuery = query(dbRef, getOrder(type), ...queries)
+      const dbQuery = query(dbRef, getOrder(type, child), ...queries)
       const dbGet = await get(dbQuery)
       const dbValue = dbGet.val()
       const dbExist = dbGet.exists()
