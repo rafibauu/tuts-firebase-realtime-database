8284a177ccf8934819ec57d9bbfea5f3ecaa3255
Rafi Bagaskara <rafibauu@gagasimaji.com>
2022-10-24 20:36:55 +0700

    Episode 18

--- services/firebase-sdk.js
+++ services/firebase-sdk.js
@@ -5,7 +5,6 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
@@ -15,5 +14,18 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth(firebaseApp)
 export const database = getDatabase(firebaseApp)
+
+export const defaultDatabase = getDatabase(
+  firebaseApp,
+  process.env.NEXT_PUBLIC_DATABASE_URL_DEFAULT
+)
+export const clientABCDatabase = getDatabase(
+  firebaseApp,
+  process.env.NEXT_PUBLIC_DATABASE_URL_CLIENT_ABC
+)
+export const clientBACDatabase = getDatabase(
+  firebaseApp,
+  process.env.NEXT_PUBLIC_DATABASE_URL_CLIENT_BAC
+)
 
 export default firebaseApp
